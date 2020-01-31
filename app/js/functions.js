0a122ee77e153f55f68ccd97d500e2a33c0ee890
Eduardo Ortega Naranjo <eduardo.ortega102@alu.ulpgc.es>
2020-01-31 19:19:50 +0000

    Add contact modal dialog.

---
+++ app/js/functions.js
@@ -0,0 +1,37 @@
+
+function hideElement(element) {
+    element.style.display = 'none';
+}
+
+
+function openModal(id) {
+    var modal = document.getElementById(id);
+    modal.style.display = 'flex';
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            hideElement(modal);
+        }
+    }
+}
+
+
+function closeModal(id) {
+    hideElement(document.getElementById(id));
+}
+
+
+function copyToClipBoard(elementId, reporterId) {
+    var element = document.getElementById(elementId);
+    element.select();
+    document.execCommand('copy');
+    element.selectionEnd = element.selectionStart;
+    element.blur();
+
+    var reporterElement = document.getElementById(reporterId);
+    reporterElement.style.opacity = 1;
+    reporterElement.style.visibility = 'visible';
+    setTimeout(() => {
+        reporterElement.style.opacity = 0;
+        reporterElement.style.visibility = 'hidden';
+    }, 1500);
+}
