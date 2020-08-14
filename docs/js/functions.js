ef32e1fbf29a24f0f6a7512c0cc2a98b2b442804
Eduardo Ortega Naranjo <eduardo.ortega102@alu.ulpgc.es>
2020-08-14 16:23:30 +0100

    Add stack to projects, Refactor: Extract project handler to module.

--- docs/js/functions.js
+++ docs/js/functions.js
@@ -107,46 +107,6 @@ function scrollToElement(id) {
     }
 }
 
-
-function filterProjects(filter) {
-    document.querySelectorAll('.filter-button', '.active').forEach(button => {
-        button.classList.remove('active');
-    });
-    document.getElementById(filter).classList.add('active');
-    document.querySelectorAll('[data-categories]').forEach(project => {
-        if (filter !== 'all' && !project.dataset.categories.includes(filter)) {
-            project.classList.add('hidden');
-        } else {
-            project.classList.remove('hidden');
-        }
-    });
-}
-
-function displayProjectInfo(button, projectId) {
-    const activeClass = 'active';
-    const project = document.getElementById(projectId);
-    if (project.classList.contains(activeClass)) {
-        button.innerText = getButtonText('expand');
-        button.classList.remove(activeClass);
-        project.classList.remove(activeClass);
-    } else {
-        button.innerText = getButtonText('contract');
-        button.classList.add(activeClass);
-        project.classList.add(activeClass);
-    }
-    button.blur();
-}
-
-function getButtonText(mode) {
-    const expandMode = 'expand';
-    const isEnglish = window.location.href.includes('/en/');
-    if (isEnglish) {
-        return mode === expandMode ? 'Read more' : 'Read less';
-    }
-    return mode === expandMode ? 'Leer más' : 'Leer menos';
-}
-
-
 function toggleMenu() {
     var menu = document.getElementById('navigation-bar');
     if (menu.classList.contains('responsive')) {
